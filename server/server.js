c4ff0fd347765da34603b37e6c0ae4da37516cdd
Hussain4569 <hsn.mustafa21@gmail.com>
2023-10-03 23:11:44 +0500

    tried alternate solution to cors from stoverflow

--- server/server.js
+++ server/server.js
@@ -4,10 +4,15 @@ const express = require("express");
 const workoutRoutes = require("./routes/workouts")
 const userRoutes = require("./routes/User")
 const mongoose = require("mongoose");
-const cors = require("cors");
 
 const app = express();
-app.use(cors());
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
 
 //middleware
 app.use(express.json());
